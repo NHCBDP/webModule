fc722ef719696ee60530912607f04b18f721eb2d
文标 <liuwenbiao@cecdat.com>
2017-09-04 15:07:45 +0800

    @wbliu first commit

---
+++ src/test/java/NHCBDP/webModel/WebModelApplicationTests.java
@@ -0,0 +1,16 @@
+package NHCBDP.webModel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebModelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
